d85b23ba22af5391aa7e2c895bf401e6b7b441ad
Jocelyn Neff <jocelyn@commondecision.org>
2013-03-12 01:33:42 -0700

    question creation servlet

--- QuizWebsite/src/question/PictureQuestion.java
+++ QuizWebsite/src/question/PictureQuestion.java
@@ -49,7 +49,7 @@ public class PictureQuestion extends Question {
 	@Override
 	public String renderCreateMode() {
 		String HTML = "";
-		HTML += "<input type=\"hidden\" name=\"type\" value=\"" + Question.QUESTION_TYPE.PICTURE + "\">" + '\n';
+		HTML += "<input type=\"hidden\" name=\"type\" value=\"" + Question.QUESTION_TYPE.PICTURE.value + "\">" + '\n';
 		HTML += "<p>Picture URL: <input type=\"text\" name=\"picture_url\"></p>" + '\n';
 		HTML += "<p>Text prompt (optional):</p>" + '\n';
 		HTML += "<textarea cols=\"40\" rows=\"5\" name=\"prompt\">Enter question prompt here</textarea>" + '\n';
--- QuizWebsite/src/servlets/CreateQuestionServlet.java
+++ QuizWebsite/src/servlets/CreateQuestionServlet.java
@@ -1,11 +1,19 @@
 package servlets;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import database.MyDB;
+
+import question.*;
+import quiz.*;
+import java.util.*;
 
 /**
  * Servlet implementation class CreateQuestionServlet
@@ -33,7 +41,41 @@ public class CreateQuestionServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		int type = Integer.parseInt(request.getParameter("type"));
+		Quiz quiz = (Quiz) request.getSession().getAttribute("quiz"); //make sure this alters the one in the session
+		int number = quiz.countQuestions() + 1;
+		if (type == Question.QUESTION_TYPE.MULTI_CHOICE.value) {
+			String prompt = request.getParameter("prompt");
+			String answer = request.getParameter("correct");
+			String[] corrects = answer.split("\n");
+			String wrong = request.getParameter("wrong");
+			String[] wrongs = wrong.split("\n");
+			quiz.addQuestion(new MultiChoiceQuestion(quiz, number, prompt, Arrays.asList(corrects), Arrays.asList(wrongs)));
+
+		} else if (type == Question.QUESTION_TYPE.FILL_IN.value) {
+			String prompt = request.getParameter("prompt");
+			String answer = request.getParameter("answer");
+			quiz.addQuestion(new FillInQuestion(quiz, number, prompt, answer));
+			
+		} else if (type == Question.QUESTION_TYPE.PICTURE.value) {
+			String url = request.getParameter("picture_url");
+			String answer = request.getParameter("answer");
+			String prompt = request.getParameter("prompt"); //being null may break it
+			PictureQuestion pQ = new PictureQuestion(quiz, number, url, prompt, answer);
+			quiz.addQuestion(pQ);
+		}
+		
+		RequestDispatcher dispatch;
+		String finish = request.getParameter("finished");
+		if (finish.equals("true")) {
+			quiz.saveToDataBase(MyDB.getConnection());
+			dispatch = request.getRequestDispatcher("QuizServlet?id=" + quiz.getID());
+			request.getSession().setAttribute("quiz", null);
+		} else {
+			dispatch = request.getRequestDispatcher("create_question.jsp");
+		}
+		dispatch.forward(request, response);
+
 	}
 
 }
